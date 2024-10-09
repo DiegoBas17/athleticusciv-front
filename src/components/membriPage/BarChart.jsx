b201027c51ce0467e1d670492be587df58db4af5
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-10-09 21:48:19 +0200

    add barchart component

---
+++ src/components/membriPage/BarChart.jsx
@@ -0,0 +1,44 @@
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// Registrazione dei componenti di Chart.js
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+
+const BarChart = ({ showAtleta }) => {
+  const data = {
+    labels: ["Partite Giocate", "Assist", "Gol"],
+    datasets: [
+      {
+        label: "Totali",
+        data: [
+          showAtleta?.partiteGiocate,
+          showAtleta?.totaleAssist,
+          showAtleta?.totaleGol,
+        ],
+        backgroundColor: "rgba(54, 162, 235, 0.6)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+};
+
+export default BarChart;
