e4d1ce1ce9c7aa8fda17c5bdebcb643d3730261b
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-10-09 19:11:13 +0200

    edit membri page

--- src/components/membriPage/MembriPage.jsx
+++ src/components/membriPage/MembriPage.jsx
@@ -11,7 +11,7 @@ const MembriPage = () => {
   const [atleti, setAtleti] = useState(null);
   const navigate = useNavigate();
   const loginError = useSelector((state) => state.errors.loginError);
-  const startingAtleta = JSON.parse(localStorage.getItem("atleta"));
+  const startingAtleta = useSelector((state) => state.atleta.atleta);
   const [selectAtleta, setSelectAtleta] = useState(null);
   const showAtleta = selectAtleta ? selectAtleta : startingAtleta;
 
@@ -38,134 +38,27 @@ const MembriPage = () => {
   return (
     <Container>
       <TopBar />
-      <Row>
+      <Row className="g-2">
         <Col lg={4}>
-          <div className="pastel-color p-4 rounded-4">
+          <div className="civ-color p-4 rounded-4">
             <h2 className="text-center">Leggende dell&rsquo;Athleticus</h2>
-            <Row className="g-2">
-              {atleti?.map((membro, index) => (
-                <Col key={index} lg={12}>
-                  <div
-                    className="border-bottom d-flex justify-content-between mt-2"
-                    onClick={() => setSelectAtleta(membro)}
-                  >
-                    <p>
-                      {membro.nome} {membro.cognome}
-                    </p>
-                    <img
-                      src={membro.avatar}
-                      alt="avatar"
-                      style={{ width: "2rem", height: "2rem" }}
-                      className="rounded-circle"
-                    />
-
-                    {/* <div className="position-relative" title="Media Gol">
-                    <dotlottie-player
-                      src="https://lottie.host/d23bab93-e20a-4353-bfc6-71063ce4c657/NY8kk3quhl.json"
-                      background="transparent"
-                      speed="1"
-                      style={{ width: "5rem", height: "5rem" }}
-                      loop
-                      autoplay
-                    ></dotlottie-player>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 100 100"
-                      width="30"
-                      height="30"
-                      style={{
-                        position: "absolute",
-                        top: "0",
-                        right: "0",
-                      }}
-                    >
-                      <circle cx="50" cy="50" r="45" fill="#FF0000" />
-                      <text
-                        x="50%"
-                        y="53%"
-                        dominantBaseline="middle"
-                        textAnchor="middle"
-                        fontSize="40"
-                        fontWeight="bold"
-                        fill="white"
-                      >
-                        {membro.mediaGol}
-                      </text>
-                    </svg>
-                  </div>
-                  <div className="position-relative" title="Media Assist">
-                    <dotlottie-player
-                      src="https://lottie.host/a7b0185d-de69-4257-9b14-56fd0ab13f1a/VBWIEYmkXY.json"
-                      ù
-                      background="transparent"
-                      speed="1"
-                      style={{ width: "5rem", height: "5rem" }}
-                      loop
-                      autoplay
-                    ></dotlottie-player>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 100 100"
-                      width="30"
-                      height="30"
-                      style={{
-                        position: "absolute",
-                        top: "0",
-                        right: "0",
-                      }}
-                    >
-                      <circle cx="50" cy="50" r="45" fill="#FF0000" />
-                      <text
-                        x="50%"
-                        y="53%"
-                        dominantBaseline="middle"
-                        textAnchor="middle"
-                        fontSize="40"
-                        fontWeight="bold"
-                        fill="white"
-                      >
-                        {membro.mediaAssist}
-                      </text>
-                    </svg>
-                  </div>
-                  <div className="position-relative" title="Media Voti">
-                    <dotlottie-player
-                      src="https://lottie.host/f64a0278-6714-4350-bc5c-65011e006e8f/idMpk5d1nt.json"
-                      background="transparent"
-                      speed="1"
-                      style={{ width: "5rem", height: "5rem" }}
-                      loop
-                      autoplay
-                    ></dotlottie-player>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 100 100"
-                      width="30"
-                      height="30"
-                      style={{
-                        position: "absolute",
-                        top: "0",
-                        right: "0",
-                      }}
-                    >
-                      <circle cx="50" cy="50" r="45" fill="#FF0000" />
-                      <text
-                        x="50%"
-                        y="53%"
-                        dominantBaseline="middle"
-                        textAnchor="middle"
-                        fontSize="40"
-                        fontWeight="bold"
-                        fill="white"
-                      >
-                        {membro.mediaVoti}
-                      </text>
-                    </svg>
-                  </div> */}
-                  </div>
-                </Col>
-              ))}
-            </Row>
+            {atleti?.map((membro, index) => (
+              <div
+                key={index}
+                className="border-bottom d-flex justify-content-between mt-2"
+                onClick={() => setSelectAtleta(membro)}
+              >
+                <p>
+                  {membro.nome} {membro.cognome}
+                </p>
+                <img
+                  src={membro.avatar}
+                  alt="avatar"
+                  style={{ width: "2rem", height: "2rem" }}
+                  className="rounded-circle"
+                />
+              </div>
+            ))}
           </div>
         </Col>
         <Col lg={8}>
--- src/components/membriPage/Profilo.jsx
+++ src/components/membriPage/Profilo.jsx
@@ -7,31 +7,43 @@ const Profilo = ({ showAtleta }) => {
   return (
     <div>
       {showAtleta ? (
-        <div className="saturated-color p-4 rounded-4">
+        <div className="civ-color p-4 rounded-4">
           <h1>Profilo</h1>
-          <h2>
-            {showAtleta.nome} {showAtleta.cognome}
-          </h2>
-          <Row>
-            <Col>
+
+          <Row className="g-2">
+            <Col lg={4}>
+              <h2>
+                {showAtleta.nome} {showAtleta.cognome}
+              </h2>
               <img
                 src={showAtleta.avatar}
                 alt="avatar"
-                style={{ width: "100%" }}
+                style={{ width: "50%" }}
                 className="rounded-circle"
               />
-              <p>Membro CIV: {showAtleta.ruolo}</p>
-              <p>email: {showAtleta.email}</p>
-              <p>numero di telefono: {showAtleta.numeroTelefono}</p>
-              <p>partite giocate: {showAtleta.partiteGiocate}</p>
-              <p>assist: {showAtleta.totaleAssist}</p>
-              <p>gol: {showAtleta.totaleGol}</p>
             </Col>
-            <Col>
-              <div
-                style={{ height: "20rem" }}
-                className="dark-color rounded-4 p-3"
-              >
+            <Col lg={6}>
+              <div>
+                <h3>Dati:</h3>
+                <div className="border border-1 rounded-4 p-2">
+                  <p>Membro CIV: {showAtleta.ruolo}</p>
+                  <p>email: {showAtleta.email}</p>
+                  <p>numero di telefono: {showAtleta.numeroTelefono}</p>
+                </div>
+              </div>
+            </Col>
+            <Col lg={6}>
+              <div className="border border-1 rounded-4 p-2">
+                <p>partite giocate: {showAtleta.partiteGiocate}</p>
+                <p>assist: {showAtleta.totaleAssist}</p>
+                <p>gol: {showAtleta.totaleGol}</p>
+                <p>media gol: {showAtleta.mediaGol}</p>
+                <p>media assist: {showAtleta.mediaAssist}</p>
+                <p>media voti: {showAtleta.mediaVoti}</p>
+              </div>
+            </Col>
+            <Col lg={6}>
+              <div style={{ height: "20rem" }} className="rounded-4 p-3">
                 <RadarChart showAtleta={showAtleta} />
               </div>
             </Col>
