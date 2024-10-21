69ce30d2c331c951ef9ee189e43781588bb804b9
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-10-21 23:55:17 +0200

    edit hero home

--- src/components/home/HeroHome.jsx
+++ src/components/home/HeroHome.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "react-bootstrap";
 
 const HeroHome = () => {
   return (
-    <Carousel controls={false} fade autoplay={true}>
+    <Carousel controls={false} fade>
       <Carousel.Item interval={5000}>
         <img
           src="https://images.unsplash.com/photo-1431324155629-1a6deb1dec8d?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
