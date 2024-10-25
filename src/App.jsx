5d9c5ad7fcb07528b7f7ce8da4c9d0ef646eba88
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-10-25 16:16:29 +0200

    edit css and font

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginRegistrazionePage from "./components/login e registrazione/LoginRegistrazionePage";
